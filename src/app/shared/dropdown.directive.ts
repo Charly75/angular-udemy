f151612abef43aa41e204c81b39220f466d8f8ef
Charles de LR <charlesdelr@gmail.com>
2021-03-20 20:40:28 +0100

    delete log

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -7,7 +7,6 @@ import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core'
     @HostBinding('class.open') isOpen = false;
     @HostListener('document:click', ['$event']) toggleOpen(event: Event): void {
       this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
-      console.log('cro ');
     }
     constructor(private elRef: ElementRef) {}
   }
